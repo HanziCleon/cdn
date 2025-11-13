245b2d04bdd54c5f9572534020a52119d02232f5
HanziCleon <137362532+HanziCleon@users.noreply.github.com>
2025-11-13 10:57:53 +0700

    Upload settings/config.js

---
+++ settings/config.js
@@ -0,0 +1,17 @@
+import fs from "fs";
+import chalk from "chalk";
+
+global.owner = ["628x","22@lid"];
+global.mode = false;
+global.menuStyle = 'relay';
+
+global.namebotz = "Alifatah wabot !";
+global.packname = 'www.ziihost.store';
+global.nameown = "Fauzialifatah | Projects";
+global.author = 'https://www.github.com/Alifatahfauzi';
+global.footer = "𝗍𝖾𝗅𝖾𝗀𝗋𝖺𝗆: @FauziAlifatah";
+
+global.YouTube = "https://www.youtube.com/@Fauzialifatah";
+global.GitHub = "https://github.com/Alifatahfauzi";
+global.Telegram = "https://t.me/FauziAlifatah";
+global.ChannelWA = "https://whatsapp.com/channel/0029VawsCnQ9mrGkOuburC1z";
